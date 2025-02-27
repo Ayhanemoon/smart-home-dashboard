9a10db4d4ca23cf8e05eda680713631f24624afb
ayhan.samimi <ayhan.samimi@gmail.com>
2025-02-27 10:07:57 +0330

    feat: bootstrap config variables

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,19 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
+  vite: {
+    css: {
+     preprocessorOptions: {
+      scss: {
+       additionalData: `
+            @use "@/assets/scss/_variables.scss" as *;
+            `,
+      },
+     },
+    }
+  },
+  css: [
+    'bootstrap/dist/css/bootstrap.css'
+  ],
   devtools: { enabled: true }
 })
