bd2f51102508a95411119b378388a382fcb46f2c
ayhan.samimi <ayhan.samimi@gmail.com>
2025-02-27 10:22:28 +0330

    feat: pinia config added

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
+  modules: [
+    '@pinia/nuxt'
+  ],
+  runtimeConfig: {
+    public: {
+      apiBase: process.env.API_BASE_URL || 'http://localhost:8000'
+    }
+  },
   vite: {
     css: {
      preprocessorOptions: {
