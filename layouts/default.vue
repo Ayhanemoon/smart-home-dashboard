5913c9c9ef6150dacbc680e1f0be91bfae4aed93
ayhan.samimi <ayhan.samimi@gmail.com>
2025-02-27 10:17:39 +0330

    feat: app layout and pages added

---
+++ layouts/default.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <header class="bg-primary text-light text-center py-3">
+      <a class="navbar-brand" href="/">Smart Home Dashboard</a>
+    </header>
+    <main>
+      <NuxtPage />
+    </main>
+    <footer class="bg-light text-center py-3">
+      © 2025 Smart Home Dashboard
+    </footer>
+  </div>
+</template>
