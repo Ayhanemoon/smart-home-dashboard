04670b17b7910c94ea6e78dd9e1a4bb9a122ddc7
ayhan.samimi <ayhan.samimi@gmail.com>
2025-02-27 16:19:23 +0330

    feat: fontawesome added

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,23 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
+  app: {
+    head: {
+      charset: 'utf-8',
+      viewport: 'width=device-width, initial-scale=1',
+      link: [
+        {
+          rel: 'stylesheet',
+          href: 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css',
+          crossorigin: 'anonymous',
+          referrerpolicy: 'no-referrer'
+        }
+      ]
+    },
+  },
+  routeRules: {
+    '/rooms': { redirect: '/' }
+  },
   modules: [
     '@pinia/nuxt'
   ],
